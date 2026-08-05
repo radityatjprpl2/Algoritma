986e606644c28b2d9681f1b36ce9c8ce02146e96
radityatjprpl2 <radityatjprpl2@gmail.com>
2026-08-05 12:39:22 +0700

    Add files via upload

---
+++ Bab3.py
@@ -0,0 +1,21 @@
+# tipe data, variabel, & operator
+
+a = "raditya"
+b = 16
+c = 162
+d = True
+
+print("nama  : ", a)
+print("umur  : ", b)
+print("tinggi: ", c, "cm")
+print("lulus : ", d)
+
+tas = "tas sekolah"
+stok = 12
+harga = 250
+ready = True
+
+print("Nama Produk     :", tas)
+print("Stok Barang     :", stok)
+print("Harga Barang RP:", harga)
+print("Tersedia Di Toko:", ready)
