aec24b0ee8bd0f5e8d6a4215903c473b5dbecc08
radityatjprpl2 <radityatjprpl2@gmail.com>
2026-08-05 08:22:38 +0700

    Add files via upload

---
+++ program_pertamaku.py
@@ -0,0 +1,7 @@
+print(10 + 5)   # penjumlahan
+print(20 - 8)   # pengurangan
+print(4 * 7)    # perkalian
+print(15 / 4)   # pembagian
+print(15 // 4)  # pembagian bulat
+print(15 % 4)   # sisa bagi
+print(2 ** 8)   # perpangkatan
