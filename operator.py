3dc3501c2f6eb61583a3b565e9c7738477e9ac02
radityatjprpl2 <radityatjprpl2@gmail.com>
2026-08-12 11:16:03 +0700

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,16 @@
+# contoh menggabungkan operator
+pjok = 70
+nilai = input("masukan nilai anda")
+hadir = True
+
+# menggunalan operator aritmatika
+nilai akhir = pjok + nilai
+print(nilai_akhir)
+
+#lulus jika nilai >= 75 dan hadir
+lulus = nilai >= 75 and hadir
+print("Lulus?", lulus)      #true
+
+# memdapat hadiah juara jika nilai >= 90 ATAU juara lomba 17 agustusan
+beasiswa = nilai >= 90 or pjok > 55
+print("Dapat hadiah juara?", hadiah juara)
