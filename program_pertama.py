0371aabfec5eacd2efccb8b3dfb531b2ea91b849
radityatjprpl2 <radityatjprpl2@gmail.com>
2026-08-12 07:39:18 +0700

    Add files via upload

---
+++ program_pertama.py
@@ -0,0 +1,19 @@
+# program pertamaku
+# Nama: Raditya dhafka al-haqi
+# kelas: x RPL2
+
+print("===============================")
+print("   SELAMAT DATANG DI PYTHON")
+print("===============================")
+print()
+print("program ini dibuat oleh", "raditya")
+print("sekolah smk tjp tuban")
+print("tahun:", 2026)
+print()
+print(10 + 5)   # penjumlahan
+print(20 - 8)   # pengurangan
+print(4 * 7)    # perkalian
+print(15 / 4)   # pembagian
+print(15 // 4)  # pembagian bulat
+print(15 % 4)   # sisa bagi
+print(2 ** 8)   # perpangkatan
